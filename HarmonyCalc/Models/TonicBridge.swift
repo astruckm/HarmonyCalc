e35c4b7abdba84985c75a88294e439f46b9c438d
Andrew Struck-Marcell <astruckm@yahoo.com>
2026-09-11 23:40:43 +0900

    Clean up unused HarmonyTypes

--- HarmonyCalc/Models/TonicBridge.swift
+++ HarmonyCalc/Models/TonicBridge.swift
@@ -15,3 +15,18 @@ func pitchClass(from noteClass: NoteClass) -> PitchClass? {
 func spellingComplexity(of noteClass: NoteClass) -> Int {
     return abs(Int(noteClass.accidental.rawValue))
 }
+
+enum TonalChordInversion: String, CaseIterable {
+    case root = "Root"
+    case first = "1st"
+    case second = "2nd"
+    case third = "3rd"
+    case fourth = "4th"
+    case fifth = "5th"
+    case sixth = "6th"
+    
+    init(inversionIndex: Int) {
+        let index = inversionIndex % TonalChordInversion.allCases.count
+        self = TonalChordInversion.allCases[index]
+    }
+}
